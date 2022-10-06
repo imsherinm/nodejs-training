d95effd33fa3fcfe54da4f4041126b84f65f33fc
adhil-bit <adhilashraf76@gmail.com>
2022-10-06 09:33:24 +0530

    adding 1 assignment file

---
+++ adil/nodejs/http.js
@@ -0,0 +1,10 @@
+const http = require("http");;
+const getOrderData = require("/home/adhil/nodejs-training/adil/nodejs/orders.json");
+
+const server =http.createServer{(req,res) => (
+    try {
+        
+    } catch (error) {
+        console.log(error);
+    }
+)
