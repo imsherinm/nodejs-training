ba4b45367904c1a82f2c6db6ae84a793d5738521
adhil@123 <67473659+adhil-bit@users.noreply.github.com>
2022-10-07 10:59:44 +0530

    otp workouts
    assignment friday

---
+++ adil/otp-project/otp_gen/index.js
@@ -0,0 +1,9 @@
+ //const generateCode = require('random-code-generate')
+ //let otp = generateRand.generateOtp()
+ ///console.log(otp);
+
+ ///var OTPGen = require('otpgen');
+ 
+//OTPGen.getOTP(5, function(otp) {
+    //console.log(JSON.stringify(otp));
+//});
