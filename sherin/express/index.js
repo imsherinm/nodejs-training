158b4935fb6efd3a5f8c2c6bb542345d3cad8be2
sherinm-dataspot <sherin@dataspot.in>
2022-10-15 11:24:54 +0530

    2022-10-15 express middleware

--- sherin/express/index.js
+++ sherin/express/index.js
@@ -1,22 +1,85 @@
-
 const express = require('express');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-
-app.listen(5000, () => {
-    console.log('server is running at port 5000');
+app.listen(3000, () => {
+    console.log('server is running ');
 });
 
-app.get('/endpoint', (req, res) => {
-    reportError.send('helo an arror');
-    
+// http methods
+
+// endpoints
+
+// domain:5000/v1/profile
+
+// read - GET
+
+// write - POST
+
+// update - PUT
+
+// delete -  DELETE
+
+const middle1 = (req, res, next) => {
+    console.log('hit on middleware 1');
+    if (req.headers.language === 'english') {
+        res.status(400).send('we dont handle english requests');
+    }
+    next();
+};
+
+app.use(middle1);
+
+app.get('/', (req, res) => {
+    try {
+        // console.log('here is the get method handler');
+        res.send('home');
+        // console.log(req.body);
+        // res.send({ name: 'sherin', age: 10, status: 'active' });
+        //
+    } catch (error) {
+        console.log('error: ', error);
+        // res.sendStatus(500).send('server side error');
+    }
 });
 
-app.post('/save/:id', (req, res) => {
-    response.send('heloi adhil here');
+// app.post('/profile', (req, res) => {
+//     try {
+//         console.log('this is second profile endpoint');
+//     } catch (error) {
+//         console.log('error: ', error);
+//     }
+// });
+
+const middle = (req, res, next) => {
+    console.log('hit on middleware');
+    req.headers.date = new Date().toLocaleDateString();
+    next();
+};
+
+app.post('/save/:id/file/:fileName', middle, (req, res) => {
+    try {
+        console.log('hit on handler');
+        console.log('headers', req.headers);
+        // console.log('body', req.body);
+        // console.log('params', req.params);
+        // console.log('query', req.query);
+        res.send();
+    } catch (error) {
+        console.log('error: ', error);
+    }
 });
+
+// app.all('*', (req, res) => {
+//     try {
+//         res.status(404).send('the page you are looking is not found');
+//     } catch (error) {
+//         console.log('error: ', error);
+//     }
+// });
 
 // app.get
 // app.post
